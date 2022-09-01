5194373d8662799c8a0fc384a6b40b6b2f27517d
Krisetn Nestler <kristen.nestler@gmail.com>
2022-08-31 21:36:34 -0400

    I3 complete

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -16,4 +16,25 @@ class Dealership
     @inventory << car
   end
 
+  def has_inventory?
+    @inventory.count != 0
+  end
+
+  def cars_by_make(make)
+    @inventory.find_all do |car| 
+      car.make == make
+    end
+  end
+
+  def total_value
+    value = []
+    @inventory.each do |car|
+      value << car.total_cost
+    end
+    value.sum
+  end
+
+  def details
+    {"total_value" => total_value, "address" => @address}
+  end
 end
