9a8d82d7cc57554a67ba3eb9754f71064eee6112
Krisetn Nestler <kristen.nestler@gmail.com>
2022-08-31 23:00:19 -0400

    I4 1 of 3 complete final submission

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -37,4 +37,25 @@ class Dealership
   def details
     {"total_value" => total_value, "address" => @address}
   end
+
+  # def average_price_of_car
+  #   # require 'pry'; binding.pry
+  #   (total_value / inventory.count).to_s.chars.insert(",")#.reverse.map_slice(3).map(&:join).join(',').reverse
+
+
+  # end
+
+  def cars_sorted_by_price
+    @inventory.sort_by {|car| car.total_cost}
+  end
+
+  # def inventory_hash
+  #   # require 'pry'; binding.pry
+  #   hash = {}
+  #     inventory.flat_map do |car| 
+  #       [car.make, car]
+  #     end
+    
+  # end
+
 end
