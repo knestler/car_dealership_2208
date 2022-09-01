6aef1157838799fc13ba8b02a290ae5e9890d06c
Krisetn Nestler <kristen.nestler@gmail.com>
2022-08-31 20:59:02 -0400

    feat: add color paint and test to car for I1

--- lib/car.rb
+++ lib/car.rb
@@ -1,18 +1,22 @@
 class Car
 
-  attr_reader :name, :make, :model, :monthly_payment, :loan_length
+  attr_reader :name, :make, :model, :monthly_payment, :loan_length, :color
 
-  def initialize(name, monthly_payment, loan_length)
+  def initialize(name, monthly_payment, loan_length, color = nil)
     @name = name
     @make = name.split[0]
     @model = name.split[1]
     @monthly_payment = monthly_payment
     @loan_length = loan_length
+    @color = nil
   end
 
   def total_cost
     @monthly_payment * loan_length
   end
 
+  def paint!(choice)
+    @color = choice
+  end
 
 end
